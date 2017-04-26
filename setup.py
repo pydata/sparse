07dbb42b5f3d96258f7ebb7daf66582ae451cbcf
Matthew Rocklin <mrocklin@gmail.com>
2017-04-26 18:36:32 -0400

    bump version to 0.1.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 
 
 setup(name='sparse',
-      version='0.0.1',
+      version='0.1.0',
       description='Sparse',
       url='http://github.com/mrocklin/sparse/',
       maintainer='Matthew Rocklin',
