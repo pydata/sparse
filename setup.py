a7234bc3f9d3e84e6af0f48847fd0383e3c5b1cd
Matthew Rocklin <mrocklin@gmail.com>
2017-08-04 16:11:48 -0700

    bump version to 0.1.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 
 
 setup(name='sparse',
-      version='0.1.0',
+      version='0.1.1',
       description='Sparse',
       url='http://github.com/mrocklin/sparse/',
       maintainer='Matthew Rocklin',
--- sparse/__init__.py
+++ sparse/__init__.py
@@ -1,3 +1,3 @@
 from .core import COO, tensordot, concatenate, stack, dot
 
-__version__ = '0.1.0'
+__version__ = '0.1.1'
