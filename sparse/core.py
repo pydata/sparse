8984c0deb8ece3e4a8b6b6117e2737c157dc9110
Alexander Bigerl <bigerl@informatik.uni-leipzig.de>
2017-08-18 15:29:03 +0200

    check input to __setitem__ and added tests for it

--- sparse/core.py
+++ sparse/core.py
@@ -287,22 +287,49 @@ class COO(object):
         Sets the single field at index to the given value.
         Parameters
         ----------
-        index : tuple
-            The coordinates of the field to write the new value to. Must be a tuple.
+        index : tuple of int
+            The coordinates of the field to write the new value to. Slices are not jet supported.
         value
             the value to write
         """
-        # TODO: check input
-        # search for index in coords and get it's position if present
+        # check input
+        # convert single valued inputs to tuples
+        if not isinstance(index, tuple):
+            index = (index,)
+        # TODO: remove this for loop when slices are supported
+        for ind in index:
+            if isinstance(ind, slice):
+                raise NotImplementedError("Slices are not jet supported for setting values.")
+        # check that all entries are Integral or slice
+        for ind in index:
+            if not isinstance(ind, numbers.Integral) and not isinstance(ind, slice):
+                raise ValueError("no valid coordinate.")
+        # check that the index has as many entries as the COO dimensions
+        if len(index) != self.ndim:
+            raise ValueError("Coordinates don't match the dimension " + str(self.ndim) + "of COO.")
+        # check that the index entries are in range of the dimension's ranges
+        for dim, ind in enumerate(index):
+            if isinstance(ind, numbers.Integral):
+                if not 0 <= ind < self.shape[dim]:
+                    raise ValueError(
+                        "Coordinate " + str(ind) + "for dimension " + str(dim) + " exceeds dimensions range of" + str(
+                            self.shape[dim]) + ".")
+
+        # convert to coords dtype
+        index = np.asarray(index, dtype=self.coords.dtype)
+
         # TODO: speedup by not checking every possible position in every dimension
+        # search for index in coords and get it's position in coords if present
         coord_ids = None
         for i, ind in enumerate(index):
-            # keep only the positions that occure in every dimension
-            coord_ids_i = np.asarray(np.where(self.coords[i] == ind)[0],dtype=self.coords.dtype)
+            # keep only the positions that occur in every dimension
+            coord_ids_i = np.where(self.coords[i] == ind)[0]
             if coord_ids is not None:
-                coord_ids = np.asarray(np.intersect1d(coord_ids, coord_ids_i),dtype=self.coords.dtype)
+                coord_ids = np.intersect1d(coord_ids, coord_ids_i)
             else:
                 coord_ids = coord_ids_i
+        # convert to self.coords dtype
+        coord_ids = np.asarray(coord_ids, dtype=self.coords.dtype)
 
         # found an value for index, replace it
         if len(coord_ids) == 1:
@@ -313,17 +340,18 @@ class COO(object):
             # TODO: test writing zeros
             # remove entry that should be set to zero
             else:
-                self.coords = np.delete(self.coords, coord_ids[0], 1) #keeps dtype type
+                self.coords = np.delete(self.coords, coord_ids[0], 1)  # keeps dtype type
                 self.data = np.delete(self.data, coord_ids[0], 0)
 
         # found no value for index, append a new non-zero value
         elif len(coord_ids) == 0:
-            # only take action if a non-zero value shoud be set
-            #  this adds a new value
+            # only take action if a non-zero value should be set
+            # this adds a new value
             if (value != 0):
-                addIndex = np.asarray([[index[i]] for i in range(len(index))],dtype=self.coords.dtype)
+                addIndex = np.asarray([[index[i]] for i in range(len(index))])  # is already self.coords.dtype
                 self.coords = np.concatenate((self.coords, addIndex), axis=1)
                 self.data = np.concatenate((self.data, [value]))
+                # TODO: add support to insert preserving sort
                 self.sorted = False
 
         else:
--- sparse/tests/test_core.py
+++ sparse/tests/test_core.py
@@ -506,6 +506,49 @@ def test_caching():
     assert len(x._cache['reshape']) < 5
 
 
+def test___setitem__input1():
+    # assign to single dimensional
+    sd = random_x((5,))
+    ssdd = COO.from_numpy(sd)
+    sd_val = random.randint(-1, 1)
+    sd[3] = sd_val
+    ssdd[3] = sd_val
+    assert_eq(sd, ssdd)
+
+
+def test___setitem__input2():
+    with pytest.raises(NotImplementedError):
+        # TODO: remove this for loop when slices are supported
+        # slices sould raise an exception
+        sl = random_x((5,))
+        ssll = COO.from_numpy(sl)
+        ssll[0:1] = [1, 1]
+
+
+def test___setitem__input3():
+    with pytest.raises(ValueError):
+        # none numbers.Integral and slices should raise an exception
+        wv = random_x((5, 5))
+        wwvv = COO.from_numpy(wv)
+        wwvv["a"] = 3
+
+
+def test___setitem__input4():
+    with pytest.raises(ValueError):
+        # indices of wrong length should raise an exception
+        wl = random_x((5, 5))
+        wwll = COO.from_numpy(wl)
+        wwll[1] = 3
+
+
+def test___setitem__input5():
+    with pytest.raises(ValueError):
+        # indices with entries out of their dimensions range should raise an exception
+        dr = random_x((5, 5))
+        ddrr = COO.from_numpy(dr)
+        ddrr[10] = 3
+
+
 def test___setitem__():
     for i in range(100):
         x = random_x((2, 3, 4))
@@ -515,30 +558,18 @@ def test___setitem__():
         xx[1, 2, 3] = ranNumber
         assert_eq(x, xx)
 
-def test_reshape_ok():
+
+def test_reshape_with__setitem__():
+    # exception is thrown if COO.coords.dtype is not minimal, see: COO.linear_loc
     dims = 3
-    coords = np.asarray([[0],[1],[1]])
-    data = np.asarray([True], dtype=bool)
+    coords = np.asarray([])
+    data = np.asarray([], dtype=bool)
     shape = (2,) * dims
     x = COO(coords=coords, data=data, shape=shape)
     new_shape1 = (4,) * dims
     y = x.reshape(shape=new_shape1)
 
-    new_shape2 = (8,) * dims
-    z = y.reshape(shape=new_shape2)
-    print("")
-
-def test_reshape():
-    dims = 3
-    coords = np.asarray([])
-    data =  np.asarray([],dtype=bool)
-    shape = (2,)*dims
-    x = COO(coords=coords,data=data,shape=shape)
-    new_shape1 = (4,)*dims
-    y = x.reshape(shape=new_shape1)
-
-    y[1,2,3] = True
+    y[1, 2, 3] = True
 
     new_shape2 = (8,) * dims
     z = y.reshape(shape=new_shape2)
-    print("")
