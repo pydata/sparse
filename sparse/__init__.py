462f606580e076967a0db53dc728aa9a04251427
Nils Werner <nils.werner@gmail.com>
2018-01-02 18:37:47 +0100

    Add random() function (#41)
    * Add random() function
    
    * random(): removed unused nnz variable
    
    * random(): random_state parameter
    
    * Replaced random_x with sparse.random
    
    * higher density in test_bitwise_binary_bool
    
    * Fixed random array cast to int, added comments why non-default random parameters are needed
    
    * Lower precision for float16 reductions
    
    * fixed flake8 errors
    
    * Add canonical_order kwarg and add tests.
    
    * Remove dtype arguent because it did nothing.
    
    * random(): data_rvs parameter
    
    * random(): Example call with data_rvs and random_state
    
    * Add remove duplicate data generation.
    
    * Fix Python 2.7 tests.
    
    * Indices must be integers, not floats
    
    * Passed through all kwargs instead of specific ones.
    
    * Less aggressive dtype. May save memory in a lot of cases.

--- sparse/__init__.py
+++ sparse/__init__.py
@@ -1,3 +1,3 @@
-from .core import COO, tensordot, concatenate, stack, dot, triu, tril
+from .core import COO, tensordot, concatenate, stack, dot, triu, tril, random
 
 __version__ = '0.1.1'
--- sparse/core.py
+++ sparse/core.py
@@ -160,6 +160,9 @@ class COO(object):
             else:
                 shape = ()
 
+        if isinstance(shape, numbers.Integral):
+            shape = (shape,)
+
         self.shape = tuple(shape)
         if self.shape:
             dtype = np.min_scalar_type(max(self.shape))
@@ -1522,3 +1525,95 @@ def _grouped_reduce(x, groups, method, **kwargs):
     result = method.reduceat(x, inv_idx, **kwargs)
     counts = np.diff(np.concatenate((inv_idx, [len(x)])))
     return result, inv_idx, counts
+
+
+def random(
+    shape,
+    density=0.01,
+    canonical_order=False,
+    random_state=None,
+    data_rvs=None,
+):
+    """ Generate a random sparse multidimensional array
+
+    Parameters
+    ----------
+    shape: :obj:`tuple` of :obj:`int`
+        Shape of the array
+    density: :obj:`float`, optional
+        Density of the generated array.
+    canonical_order : bool, optional
+        Whether or not to put the output :obj:`COO` object into canonical
+        order. :code:`False` by default.
+    random_state : {numpy.random.RandomState, int}, optional
+        Random number generator or random seed. If not given, the
+        singleton numpy.random will be used. This random state will be used
+        for sampling the sparsity structure, but not necessarily for sampling
+        the values of the structurally nonzero entries of the matrix.
+    data_rvs : callable
+        Data generation callback. Must accept one single parameter: number of
+        :code:`nnz` elements, and return one single NumPy array of exactly
+        that length.
+
+    Returns
+    -------
+    COO
+        The generated random matrix.
+
+    See Also
+    --------
+    :obj:`scipy.sparse.rand`
+        Equivalent Scipy function.
+
+    Examples
+    --------
+
+    >>> from sparse import random
+    >>> from scipy import stats
+    >>> rvs = lambda x: stats.poisson(25, loc=10).rvs(x, random_state=np.random.RandomState(1))
+    >>> S = random((2, 3, 4), density=0.25, random_state=np.random.RandomState(1), data_rvs=rvs)
+    >>> S.todense()
+    array([[[ 0,  0,  0,  0],
+            [ 0, 34,  0,  0],
+            [33, 34,  0, 29]],
+    <BLANKLINE>
+           [[30,  0,  0, 34],
+            [ 0,  0,  0,  0],
+            [ 0,  0,  0,  0]]])
+
+    """
+    # Copied, in large part, from scipy.sparse.random
+    # See https://github.com/scipy/scipy/blob/master/LICENSE.txt
+
+    elements = np.prod(shape)
+
+    nnz = int(elements * density)
+
+    if random_state is None:
+        random_state = np.random
+    elif isinstance(random_state, numbers.Integral):
+        random_state = np.random.RandomState(random_state)
+    if data_rvs is None:
+        data_rvs = random_state.rand
+
+    # Use the algorithm from python's random.sample for k < mn/3.
+    if elements < 3 * nnz:
+        ind = random_state.choice(elements, size=nnz, replace=False)
+    else:
+        ind = np.empty(nnz, dtype=np.min_scalar_type(elements - 1))
+        selected = set()
+        for i in range(nnz):
+            j = random_state.randint(elements)
+            while j in selected:
+                j = random_state.randint(elements)
+            selected.add(j)
+            ind[i] = j
+
+    data = data_rvs(nnz)
+
+    ar = COO(ind[None, :], data, shape=nnz).reshape(shape)
+
+    if canonical_order:
+        ar.sum_duplicates()
+
+    return ar
