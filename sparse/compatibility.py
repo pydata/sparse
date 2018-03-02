445c7c3f05477fbf75b12047e12e88b5788ba7e9
Hameer Abbasi <hameerabbasi@yahoo.com>
2018-03-02 15:01:18 +0100

    Drop alias for compatibility.py.

--- sparse/compatibility.py
+++ sparse/compatibility.py
@@ -1,15 +1,13 @@
 # flake8: noqa F401
 import sys
 
-cur_version = sys.version_info
-
-if cur_version < (2, 7):
+if sys.version_info < (2, 7):
     raise ImportError('Need at least Python 2.7.')
 
-if cur_version[0] == 3 and cur_version[1] < 5:
+if sys.version_info[0] == 3 and sys.version_info[1] < 5:
     raise ImportError('Need at least Python 3.5 if using Python 3.')
 
-if cur_version[0] == 3:
+if sys.version_info[0] >= 3:
     from itertools import zip_longest
     from builtins import int, range, zip
 else:
