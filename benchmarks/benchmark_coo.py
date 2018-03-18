dbf0a18d6df9068cb9038e1e8fdcb821f5134790
Hameer Abbasi <hameerabbasi@yahoo.com>
2018-03-18 13:35:48 +0100

    Add airspeed velocity benchmarks.

---
+++ benchmarks/benchmark_coo.py
@@ -0,0 +1,35 @@
+import numpy as np
+
+import sparse
+
+
+class ElemwiseSuite(object):
+    def setup(self):
+        np.random.seed(0)
+        self.x = sparse.random((100, 100, 100), density=0.01)
+        self.y = sparse.random((100, 100, 100), density=0.01)
+
+        self.x.sum_duplicates()
+        self.y.sum_duplicates()
+
+    def time_add(self):
+        self.x + self.y
+
+    def time_mul(self):
+        self.x * self.y
+
+
+class ElemwiseBroadcastingSuite(object):
+    def setup(self):
+        np.random.seed(0)
+        self.x = sparse.random((100, 1, 100), density=0.01)
+        self.y = sparse.random((100, 100), density=0.01)
+
+        self.x.sum_duplicates()
+        self.y.sum_duplicates()
+
+    def time_add(self):
+        self.x + self.y
+
+    def time_mul(self):
+        self.x * self.y
