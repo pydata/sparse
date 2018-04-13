Fix axes normalization bugs (#136)

sparse/coo/core.py
@@ -9,7 +9,7 @@ from .indexing import getitem
 from .umath import elemwise, broadcast_to
 from ..compatibility import int, range
 from ..sparse_array import SparseArray
-from ..utils import _zero_of_dtype
+from ..utils import _zero_of_dtype, normalize_axis
 
 
 class COO(SparseArray, NDArrayOperatorsMixin):
@@ -570,6 +570,7 @@ class COO(SparseArray, NDArrayOperatorsMixin):
         >>> s.reduce(np.add)
         <COO: shape=(5,), dtype=int64, nnz=5, sorted=True, duplicates=False>
         """
+        axis = normalize_axis(axis, self.ndim)
         zero_reduce_result = method.reduce([_zero_of_dtype(self.dtype)], **kwargs)
 
         if zero_reduce_result != _zero_of_dtype(np.dtype(zero_reduce_result)):
@@ -925,24 +926,8 @@ class COO(SparseArray, NDArrayOperatorsMixin):
         if axes is None:
             axes = list(reversed(range(self.ndim)))
 
-        # Normalize all axe indices to posivite values
-        axes = np.array(axes)
-        axes[axes < 0] += self.ndim
-
-        if np.any(axes >= self.ndim) or np.any(axes < 0):
-            raise ValueError("invalid axis for this array")
-
-        if len(np.unique(axes)) < len(axes):
-            raise ValueError("repeated axis in transpose")
-
-        if not len(axes) == self.ndim:
-            raise ValueError("axes don't match array")
-
-        # Normalize all axe indices to posivite values
-        try:
-            axes = np.arange(self.ndim)[list(axes)]
-        except IndexError:
-            raise ValueError("invalid axis for this array")
+        # Normalize all axes indices to positive values
+        axes = normalize_axis(axes, self.ndim)
 
         if len(np.unique(axes)) < len(axes):
             raise ValueError("repeated axis in transpose")

sparse/tests/test_coo.py
@@ -34,7 +34,7 @@ def test_reductions(reduction, axis, keepdims, kwargs, eqkwargs):
     (np.prod, {}, {}),
     (np.min, {}, {}),
 ])
-@pytest.mark.parametrize('axis', [None, 0, 1, 2, (0, 2)])
+@pytest.mark.parametrize('axis', [None, 0, 1, 2, (0, 2), -1, (0, -1)])
 @pytest.mark.parametrize('keepdims', [True, False])
 def test_ufunc_reductions(reduction, axis, keepdims, kwargs, eqkwargs):
     x = sparse.random((2, 3, 4), density=.5)
@@ -116,15 +116,14 @@ def test_transpose(axis):
     (0, -1, -4),  # axis -4 illegal
     (0, 0, 1),  # duplicate axis 0
     (0, -1, 2),  # duplicate axis -1 == 2
+    0.3,  # Invalid type in axis
+    ((0, 1, 2),),  # Iterable inside iterable
 ])
 def test_transpose_error(axis):
     x = sparse.random((2, 3, 4), density=.25)
-    y = x.todense()
 
     with pytest.raises(ValueError):
         x.transpose(axis)
-    with pytest.raises(ValueError):
-        y.transpose(axis)
 
 
 @pytest.mark.parametrize('a,b', [