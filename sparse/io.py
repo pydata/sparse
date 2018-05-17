2c8d94a7efb397a6f3cf749ca6d9d3c30ca306c2
NimrodM <nimrodm@way2vat.com>
2018-05-17 17:03:13 +0300

    Added docstrings

--- sparse/io.py
+++ sparse/io.py
@@ -4,6 +4,41 @@ from sparse.coo.core import COO
 
 
 def save_npz(filename, matrix, compressed=True):
+    """ Save a sparse matrix to disk in numpy's ``.npz`` format
+
+    Examples
+    --------
+    Store sparse matrix to disk, and load it again:
+
+    >>> import sparse
+    >>> import numpy as np
+    >>> dense_mat = np.array([[[0., 0.], [0., 0.70677779]], [[0., 0.], [0., 0.86522495]]])
+    >>> mat = sparse.COO(dense_mat)
+    >>> mat
+    <COO: shape=(2, 2, 2), dtype=float64, nnz=2>
+    >>> sparse.io.save_npz('mat.npz', mat)
+    >>> loaded_mat = sparse.io.load_npz('mat.npz')
+    >>> loaded_mat
+    <COO: shape=(2, 2, 2), dtype=float64, nnz=2>
+    >>> loaded_mat.todense()
+    array([[[0.        , 0.        ],
+            [0.        , 0.70677779]],
+    <BLANKLINE>
+           [[0.        , 0.        ],
+            [0.        , 0.86522495]]])
+
+    :param filename: string or file
+        Either the file name (string) or an open file (file-like object)
+        where the data will be saved. If file is a string or a Path, the
+        ``.npz`` extension will be appended to the file name if it is not
+        already there
+    :param matrix: coo matrix
+        The matrix to save to disk
+    :param compressed: bool
+        Whether to save in compressed or uncompressed mode
+
+    """
+
     nodes = {'data':   matrix.data,
              'coords': matrix.coords}
 
@@ -14,6 +49,39 @@ def save_npz(filename, matrix, compressed=True):
 
 
 def load_npz(filename):
+    """ Load a sparse matrix in numpy's ``.npz`` format from disk
+
+        Examples
+        --------
+        Store sparse matrix to disk, and load it again:
+
+        >>> import sparse
+        >>> import numpy as np
+        >>> dense_mat = np.array([[[0., 0.], [0., 0.70677779]], [[0., 0.], [0., 0.86522495]]])
+        >>> mat = sparse.COO(dense_mat)
+        >>> mat
+        <COO: shape=(2, 2, 2), dtype=float64, nnz=2>
+        >>> sparse.io.save_npz('mat.npz', mat)
+        >>> loaded_mat = sparse.io.load_npz('mat.npz')
+        >>> loaded_mat
+        <COO: shape=(2, 2, 2), dtype=float64, nnz=2>
+        >>> loaded_mat.todense()
+        array([[[0.        , 0.        ],
+                [0.        , 0.70677779]],
+        <BLANKLINE>
+               [[0.        , 0.        ],
+                [0.        , 0.86522495]]])
+
+        :param filename: file-like object, string, or pathlib.Path
+            The file to read. File-like objects must support the
+            ``seek()`` and ``read()`` methods.
+        :param matrix: coo matrix
+            The matrix to save to disk
+        :param compressed: bool
+            Whether to save in compressed or uncompressed mode
+        :return: coo matrix
+            The sparse matrix at path ``filename``
+        """
     with np.load(filename) as fp:
         try:
             coords = fp['coords']
