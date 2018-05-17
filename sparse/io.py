9a6c38e7c3bee7b2095e71f059a62083e935fcd0
NimrodM <nimrodm@way2vat.com>
2018-05-17 15:06:39 +0300

    flake8 fixes, made test python2.7 compatible

--- sparse/io.py
+++ sparse/io.py
@@ -2,6 +2,7 @@ import numpy as np
 
 from sparse.coo.core import COO
 
+
 def save_npz(filename, matrix, compressed=True):
     nodes = {'data':   matrix.data,
              'coords': matrix.coords}
@@ -11,11 +12,12 @@ def save_npz(filename, matrix, compressed=True):
     else:
         np.savez(filename, **nodes)
 
+
 def load_npz(filename):
     with np.load(filename) as fp:
         try:
             coords = fp['coords']
-            data   = fp['data']
+            data = fp['data']
         except KeyError:
             raise RuntimeError('The file {} does not contain a valid sparse matrix'.format(filename))
 
