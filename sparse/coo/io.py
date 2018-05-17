e9c735df305e1625613e31bd1f87e85c1f989f9f
NimrodM <nimrodm@way2vat.com>
2018-05-17 13:42:14 +0300

    Added save_npz and load_npz methods

---
+++ sparse/coo/io.py
@@ -0,0 +1,22 @@
+import numpy as np
+
+from .core import COO
+
+def save_npz(filename, matrix, compressed=True):
+    nodes = {'data':   matrix.data,
+             'coords': matrix.coords}
+
+    if compressed:
+        np.savez_compressed(filename, **nodes)
+    else:
+        np.savez(filename, **nodes)
+
+def load_npz(filename):
+    with np.load(filename) as fp:
+        try:
+            coords = fp['coords']
+            data   = fp['data']
+        except KeyError:
+            raise RuntimeError('The file {} does not contain a valid sparse matrix'.format(filename))
+
+    return COO(coords=coords, data=data)
