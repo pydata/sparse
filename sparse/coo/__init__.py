afd3e80b6ef0d7e8c83170e2437ee270584ef74e
ahwillia <alex.h.willia@gmail.com>
2018-06-24 14:22:50 -0700

    add sparse.roll function

--- sparse/coo/__init__.py
+++ sparse/coo/__init__.py
@@ -1,4 +1,4 @@
 from .core import COO, as_coo
 from .umath import elemwise
 from .common import tensordot, dot, concatenate, stack, triu, tril, where, \
-    nansum, nanprod, nanmin, nanmax, nanreduce
+    nansum, nanprod, nanmin, nanmax, nanreduce, roll
