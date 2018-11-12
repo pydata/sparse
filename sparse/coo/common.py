Fix overflow in reduce.

sparse/coo/common.py
@@ -7,7 +7,7 @@ import numpy as np
 import scipy.sparse
 
 from ..sparse_array import SparseArray
-from ..compatibility import range
+from ..compatibility import range, int
 from ..utils import isscalar, normalize_axis, check_zero_fill_value, check_consistent_fill_value
 
 
@@ -48,7 +48,7 @@ def asCOO(x, name='asCOO', check=True):
 def linear_loc(coords, shape):
     out = np.zeros(coords.shape[1], dtype=np.intp)
     tmp = np.zeros(coords.shape[1], dtype=np.intp)
-    strides = 1
+    strides = int(1)
     for i, d in enumerate(shape[::-1]):
         np.multiply(coords[-(i + 1), :], strides, out=tmp)
         np.add(tmp, out, out=out)

sparse/coo/core.py
@@ -697,8 +697,8 @@ class COO(SparseArray, NDArrayOperatorsMixin):
             neg_axis = tuple(ax for ax in range(self.ndim) if ax not in set(axis))
 
             a = self.transpose(neg_axis + axis)
-            a = a.reshape((np.prod([self.shape[d] for d in neg_axis]),
-                           np.prod([self.shape[d] for d in axis])))
+            a = a.reshape((np.prod([self.shape[d] for d in neg_axis], dtype=np.intp),
+                           np.prod([self.shape[d] for d in axis], dtype=np.intp)))
 
             result, inv_idx, counts = _grouped_reduce(a.data, a.coords[0], method, **kwargs)
             missing_counts = counts != a.shape[1]