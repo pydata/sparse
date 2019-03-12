313c4d57208d5d2ba02f9d4f751a119903c72031
Peter Andreas Entschev <peter@entschev.com>
2019-03-12 11:01:54 +0100

    Add __array_function__ tests

---
+++ sparse/tests/test_array_function.py
@@ -0,0 +1,22 @@
+import sparse
+from sparse.utils import assert_eq
+import pytest
+np = pytest.importorskip('numpy', minversion='1.16')
+
+
+@pytest.mark.parametrize('func', [
+    lambda x: np.dot(x, x),
+    lambda x: np.mean(x),
+    lambda x: np.sum(x),
+    lambda x: np.sum(x, axis=0),
+    lambda x: np.stack([x, x]),
+    lambda x: np.tensordot(x, x),
+    lambda x: np.transpose(x),
+    lambda x: np.where(x > np.transpose(x), x, np.transpose(x))
+])
+def test_array_function(func):
+    x = sparse.random((50, 50), density=.25)
+    y = x.todense()
+    xx = func(x)
+    yy = func(y)
+    assert_eq(xx, yy)
