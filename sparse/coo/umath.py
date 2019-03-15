Fix 1 size non-empty broadcast. (#243)
* Fix 1 size non-empty broadcast.

* Actually test.

sparse/coo/umath.py
@@ -260,7 +260,13 @@ def _get_expanded_coords_data(coords, data, params, broadcast_shape):
     dim = 0
 
     expanded_coords = np.empty((len(broadcast_shape), all_idx.shape[1]), dtype=np.intp)
-    expanded_data = data[all_idx[first_dim]]
+
+    if first_dim != -1:
+        expanded_data = data[all_idx[first_dim]]
+    else:
+        expanded_coords = all_idx
+        expanded_data = np.repeat(data, np.prod(broadcast_shape, dtype=np.int64))
+        return np.asarray(expanded_coords), np.asarray(expanded_data)
 
     for d, p, l in zip(range(len(broadcast_shape)), params, broadcast_shape):
         if p:

sparse/tests/test_coo.py
@@ -613,12 +613,16 @@ def test_elemwise_trinary(func, shape):
                                            ((1, 5), (5, 1)),
                                            ((3, 1), (3, 4)),
                                            ((3, 1), (1, 4)),
-                                           ((1, 4), (3, 4))])
+                                           ((1, 4), (3, 4)),
+                                           ((2, 2, 2), (1, 1, 1))])
 def test_binary_broadcasting(func, shape1, shape2):
-    xs = sparse.random(shape1, density=0.5)
+    density1 = 1 if np.prod(shape1) == 1 else 0.5
+    density2 = 1 if np.prod(shape2) == 1 else 0.5
+
+    xs = sparse.random(shape1, density=density1)
     x = xs.todense()
 
-    ys = sparse.random(shape2, density=0.5)
+    ys = sparse.random(shape2, density=density2)
     y = ys.todense()
 
     expected = func(x, y)