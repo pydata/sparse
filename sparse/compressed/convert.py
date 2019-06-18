02f3af96d2fc27e1ce5c427a194d85629e34056a
Dale Tovar <37719258+daletovar@users.noreply.github.com>
2019-06-17 21:02:07 -0700

    Add files via upload

--- sparse/compressed/convert.py
+++ sparse/compressed/convert.py
@@ -20,32 +20,31 @@ def convert_prep(inds,shape,axisptr):
         rows = np.empty(row_idx_size,dtype=np.intp)
         row_operations = np.prod([ind.size for ind in inds[:axisptr-1]])
         row_key_vals = [int(row_inds[i][0]) for i in range(len(row_inds[:-1]))]
-        j = np.zeros(len(row_shapes)-1,dtype=int)
-        rows = convert_to_2d(row_inds,row_key_vals,row_shapes,row_operations,rows,j)
+        positions = np.zeros(len(row_shapes)-1,dtype=int)
+        rows = convert_to_2d(row_inds,row_key_vals,row_shapes,row_operations,rows,positions)
     if len(col_shapes)==1:
         cols = col_inds[0]
     else:
         cols = np.empty(col_idx_size,dtype=int)
         col_operations = np.prod([ind.size for ind in inds[axisptr:-1]])
         col_key_vals = [int(col_inds[i][0]) for i in range(len(col_inds[:-1]))]
-        j = np.zeros(len(col_shapes)-1,dtype=int)
-        cols = convert_to_2d(col_inds,col_key_vals,col_shapes,col_operations,cols,j)
+        positions = np.zeros(len(col_shapes)-1,dtype=int)
+        cols = convert_to_2d(col_inds,col_key_vals,col_shapes,col_operations,cols,positions)
     return rows,cols
 
-#@numba.jit(nopython=True,nogil=True)
-def convert_to_2d(inds,key_vals,shape,operations,indices,j):
+def convert_to_2d(inds,key_vals,shape,operations,indices,positions):
     
     pos = len(key_vals)-1
     increment = 0
     for i in range(operations):
         if key_vals[pos] == inds[pos][-1]:
             key_vals[pos] = inds[pos][0]
-            j[pos] = 0
+            positions[pos] = 0
             pos -= 1
-            j[pos] += 1
-        key_vals[pos] = inds[pos][j[pos]]
+            positions[pos] += 1
+        key_vals[pos] = inds[pos][positions[pos]]
         pos = len(key_vals)-1
-        j[pos] +=1
+        positions[pos] +=1
         add = 0
         for count,val in enumerate(key_vals):
             other = np.prod(shape[count:-1])
@@ -55,13 +54,10 @@ def convert_to_2d(inds,key_vals,shape,operations,indices,j):
     
     return indices
 
-def uncompress_dimension(indptr,indices):
+def uncompress_dimension(indptr):
     """converts an index pointer array into an array of coordinates"""
-    uncompressed = np.empty(indices.shape[0],dtype=np.intp)
-    position = 0
+    uncompressed = np.empty(indptr[-1],dtype=np.intp)
     for i in range(len(indptr)-1):
-        inds = indices[indptr[i]:indptr[i+1]].shape[0] 
-        uncompressed[np.arange(position,inds + position)] = i
-        position += inds
+        uncompressed[indptr[i]:indptr[i+1]] = i
     return uncompressed
 
