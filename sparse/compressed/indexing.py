97d42ff0749644c5a30250a56668d5c1fce055d3
Dale Tovar <37719258+daletovar@users.noreply.github.com>
2019-07-06 01:32:33 -0700

    add working indexing

--- sparse/compressed/indexing.py
+++ sparse/compressed/indexing.py
@@ -8,7 +8,7 @@ from ..coo.common import linear_loc
 from ..coo.core import COO
 from functools import reduce
 from operator import mul
-from .convert import convert_prep,uncompress_dimension
+from .convert import convert_to_flat,uncompress_dimension
 
 
 def getitem(x,key):
@@ -16,10 +16,11 @@ def getitem(x,key):
     
     
     """
-    from .compressed import CSD
+    from .compressed import GXCS
 
     key = list(normalize_index(key,x.shape))
-        
+    
+    # return a single element
     if all(isinstance(k, int) for k in key): # indexing for a single element
         key = np.array(key)[x.axis_order] # reordering the input
         ind = np.ravel_multi_index(key,x.reordered_shape)
@@ -32,45 +33,92 @@ def getitem(x,key):
         return x.fill_value
 
     shape = []
-    compressed_inds = 0
-    uncompressed_inds = 0
-    reordered_key = copy.copy(key)
+    compressed_inds = np.zeros(len(x.shape),dtype=np.bool)
+    uncompressed_inds = np.zeros(len(x.shape),dtype=np.bool)
+    shape_key = np.zeros(len(x.shape),dtype=np.intp)
+    
+    count = 0
     for i,ind in enumerate(key):
         if isinstance(ind,Integral):
             continue
         elif isinstance(ind,slice):
+            shape_key[i] = count
             shape.append(len(range(ind.start,ind.stop,ind.step)))
             if i in x.compressed_axes:
-                compressed_inds += 1
+                compressed_inds[i] = True
             else:
-                uncompressed_inds += 1
+                uncompressed_inds[i] = True
         elif isinstance(ind, Iterable):
+            shape_key[i] = count
             shape.append(len(ind))
             if i in x.compressed_axes:
-                compressed_inds += 1
+                compressed_inds[i] = True
             else:
-                uncompressed_inds += 1
-        reordered_key[x.axis_order[i]] = key[i] # reorder the key
+                uncompressed_inds[i] = True
+        
+        count += 1
 
+    reordered_key = [key[i] for i in x.axis_order]
+
+    # find starts and ends of rows
+    a = x.indptr[:-1].reshape(x.reordered_shape[:x.axisptr])
+    b = x.indptr[1:].reshape(x.reordered_shape[:x.axisptr])
+    starts, ends = a[reordered_key[:x.axisptr]].flatten(), b[reordered_key[:x.axisptr]].flatten()
+
+    # prepare for converting to flat indices
     for i,ind in enumerate(reordered_key):
         if isinstance(ind,Integral):
             reordered_key[i] = [ind]
         elif isinstance(ind,slice):
             reordered_key[i] = np.arange(ind.start,ind.stop,ind.step)
+    
+    shape = np.array(shape)
+    
 
-    shape = tuple(shape)
-
-    # this is all temporary and currently doesn't work
-    rows,cols = convert_prep(reordered_key,x.reordered_shape,x.axisptr)
-    reordered_shape = np.array(shape)[x.axis_order] 
-    indptr = np.empty(np.prod(reordered_shape[x.axisptr])+1,dtype=np.intp)
+    cols = convert_to_flat(reordered_key,x.reordered_shape,x.axisptr)
+    
+    if np.any(compressed_inds):
+        compressed_axes = shape_key[compressed_inds]
+        
+        if len(compressed_axes)==1:
+            row_size = shape[compressed_axes]
+        else:
+            row_size = np.prod(shape[compressed_axes])     
+        
+    else: # only uncompressed axes     
+        compressed_axes = (0,) # defaults to 0
+        row_size = 1 # this doesn't matter
+        
+    if not np.any(uncompressed_inds): # only indexing compressed axes
+        compressed_axes = (0,) # defaults to 0
+        row_size = starts.size
+        
+    indptr = np.empty(row_size+1,dtype=np.intp)
     indptr[0] = 0
-    arg = get_array_selection(x.data,x.indices,x.indptr,indptr,rows,cols) 
-    return CSD(arg,shape=shape,compressed_axes=x.compressed_axes,fill_value=x.fill_value)
+    arg = get_array_selection(x.data,x.indices,indptr,starts,ends,cols)
+    shape = tuple(shape)
+    
+    data,indices,indptr = arg
+    size = np.prod(shape[1:])
+
+    if not np.any(uncompressed_inds): # only indexing compressed axes
+        uncompressed = uncompress_dimension(indptr)
+        indices = uncompressed % size
+        indptr = np.empty(shape[0]+1,dtype=np.intp)
+        np.cumsum(np.bincount(uncompressed//size,minlength=shape[0]),out=indptr[1:])
+    if not np.any(compressed_inds):
+        uncompressed = indices // size
+        indptr = np.empty(shape[0]+1,dtype=np.intp)
+        np.cumsum(np.bincount(uncompressed,minlength=shape[0]),out=indptr[1:])
+        indices = indices % size
+    
+    arg = (data,indices,indptr)
+    
+    return GXCS(arg,shape=shape,compressed_axes=compressed_axes,fill_value=x.fill_value)
 
 
 @numba.jit(nopython=True,nogil=True)
-def get_array_selection(arr_data,arr_indices,arr_indptr,indptr,row,col):
+def get_array_selection(arr_data,arr_indices,indptr,starts,ends,col):
     """
     This is a very general algorithm to be used when more optimized methods don't apply. 
     It performs a binary search for each of the requested elements. 
@@ -79,16 +127,16 @@ def get_array_selection(arr_data,arr_indices,arr_indptr,indptr,row,col):
     """
     indices = []
     ind_list = []
-    for i,r in enumerate(row):
+    for i,(start,end) in enumerate(zip(starts,ends)):
         inds = []
-        current_row = arr_indices[arr_indptr[r]:arr_indptr[r+1]]
+        current_row = arr_indices[start:end]
         if len(current_row) == 0:
             indptr[i+1] = indptr[i]
             continue
         for c in range(len(col)):
             s = np.searchsorted(current_row,col[c]) 
             if not (s >= current_row.size or current_row[s] != col[c]):
-                s += arr_indptr[r]
+                s += start
                 inds.append(s)
                 indices.append(c)
         ind_list.extend(inds)
