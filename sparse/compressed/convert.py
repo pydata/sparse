c30db2e060722d866b70e1929258292d4041858b
Dale Tovar <37719258+daletovar@users.noreply.github.com>
2019-08-08 23:54:15 -0700

    Update convert.py

--- sparse/compressed/convert.py
+++ sparse/compressed/convert.py
@@ -2,24 +2,27 @@ import numpy as np
 import numba
 from numba.typed import List
 
+
 def convert_to_flat(inds, shape, axisptr):
     inds = [np.array(ind) for ind in inds]
     if any(ind.ndim > 1 for ind in inds):
         raise IndexError('Only one-dimensional iterable indices supported.')
     uncompressed_inds = inds[axisptr:]
-    cols = np.empty(np.prod([ind.size for ind in uncompressed_inds]),dtype=np.intp)
+    cols = np.empty(
+        np.prod([ind.size for ind in uncompressed_inds]), dtype=np.intp)
     shape_bins = transform_shape(shape[axisptr:])
     increments = List()
     for i in range(len(uncompressed_inds)):
         increments.append(uncompressed_inds[i] * shape_bins[i])
     operations = np.prod([ind.shape[0] for ind in increments[:-1]])
-    return compute_flat(increments,cols,operations)
-    
-@numba.jit(nopython=True,nogil=True)
-def compute_flat(increments,cols,operations):
+    return compute_flat(increments, cols, operations)
+
+
+@numba.jit(nopython=True, nogil=True)
+def compute_flat(increments, cols, operations):
     start = 0
     end = increments[-1].shape[0]
-    positions = np.zeros(len(increments)-1,dtype=np.intp)
+    positions = np.zeros(len(increments)-1, dtype=np.intp)
     pos = len(increments)-2
     for i in range(operations):
         if i != 0 and positions[pos] == increments[pos].shape[0]:
@@ -27,25 +30,27 @@ def compute_flat(increments,cols,operations):
             pos -= 1
             positions[pos] += 1
             pos += 1
-        to_add = np.array([increments[i][positions[i]] for i in range(len(increments)-1)]).sum()
+        to_add = np.array([increments[i][positions[i]]
+                           for i in range(len(increments)-1)]).sum()
         cols[start:end] = increments[-1] + to_add
         positions[pos] += 1
         start += increments[-1].shape[0]
         end += increments[-1].shape[0]
     return cols
-        
-    
+
+
 def transform_shape(shape):
     """
     turns a shape into the linearized increments that 
     it represents. For example, given (5,5,5), it returns
     np.array([25,5,1]).
     """
-    shape_bins = np.empty(len(shape),dtype=np.intp)
+    shape_bins = np.empty(len(shape), dtype=np.intp)
     shape_bins[-1] = 1
-    for i in range(len(shape)-2,-1,-1):
+    for i in range(len(shape)-2, -1, -1):
         shape_bins[i] = np.prod(shape[i+1:])
     return shape_bins
+
 
 @numba.jit(nopython=True, nogil=True)
 def uncompress_dimension(indptr):
