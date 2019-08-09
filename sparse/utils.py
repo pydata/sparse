8bb2b562586684a8df933f686dbff662ec1a4611
Dale Tovar <37719258+daletovar@users.noreply.github.com>
2019-08-09 01:10:46 -0700

    Update utils.py

--- sparse/utils.py
+++ sparse/utils.py
@@ -43,7 +43,8 @@ def assert_eq(x, y, check_nnz=True, compare_dtype=True, **kwargs):
 
 
 def assert_nnz(s, x):
-    fill_value = s.fill_value if hasattr(s, 'fill_value') else _zero_of_dtype(s.dtype)
+    fill_value = s.fill_value if hasattr(
+        s, 'fill_value') else _zero_of_dtype(s.dtype)
     assert np.sum(~equivalent(x, fill_value)) == s.nnz
 
 
@@ -137,7 +138,8 @@ def random(
     nnz = int(elements * density)
 
     if format != 'gxcs' and compressed_axes is not None:
-        raise ValueError('compressed_axes is not supported for {} format'.format(format))
+        raise ValueError(
+            'compressed_axes is not supported for {} format'.format(format))
 
     if random_state is None:
         random_state = np.random
@@ -161,9 +163,10 @@ def random(
 
     data = data_rvs(nnz)
 
-    ar = COO(ind[None, :], data, shape=elements, fill_value=fill_value).reshape(shape)
+    ar = COO(ind[None, :], data, shape=elements,
+             fill_value=fill_value).reshape(shape)
 
-    return ar.asformat(format,compressed_axes=compressed_axes)
+    return ar.asformat(format, compressed_axes=compressed_axes)
 
 
 def isscalar(x):
@@ -209,7 +212,8 @@ def normalize_axis(axis, ndim):
             axis += ndim
 
         if axis >= ndim or axis < 0:
-            raise ValueError('Invalid axis index %d for ndim=%d' % (axis, ndim))
+            raise ValueError(
+                'Invalid axis index %d for ndim=%d' % (axis, ndim))
 
         return axis
 
@@ -259,7 +263,8 @@ def equivalent(x, y):
 
     # Can contain NaNs
     # FIXME: Complex floats and np.void with multiple values can't be compared properly.
-    return (x == y) | ((x != x) & (y != y))  # lgtm [py/comparison-of-identical-expressions]
+    # lgtm [py/comparison-of-identical-expressions]
+    return (x == y) | ((x != x) & (y != y))
 
 
 def check_zero_fill_value(*args):
