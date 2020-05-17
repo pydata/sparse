d3c3e6259629656dbed52204f99d8f61f179f5b7
lueckem <marvin-luecke@web.de>
2020-05-17 20:20:25 +0200

    added some documentation

--- benchmarks/benchmark_tensordot.py
+++ benchmarks/benchmark_tensordot.py
@@ -3,10 +3,13 @@ import sparse
 
 
 class TensordotSuite:
+    """
+    Performance comparison for returntype="sparse".
+    """
     def setup(self):
         np.random.seed(0)
         self.n = np.random.random((100, 100))
-        self.s = sparse.random((100, 100, 100, 100), density=0.001)
+        self.s = sparse.random((100, 100, 100, 100), density=0.01)
 
     def time_dense(self):
         sparse.tensordot(self.n, self.s, axes=([0, 1], [0, 2]))
