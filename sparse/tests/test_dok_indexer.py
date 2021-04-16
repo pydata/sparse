0a4be342a25813b65a3b135eef5cb8e7888a6b9d
Sayandip Halder <sayandiph4@gmail.com>
2021-04-16 16:03:18 +0530

    adding credits

--- sparse/tests/test_dok_indexer.py
+++ sparse/tests/test_dok_indexer.py
@@ -1,3 +1,19 @@
+# Copyright 2019 Steffen Schroeder
+# All the contents of the file have been copied from here
+# https://gist.github.com/stschroe/7c4660774835258c06b2be968eb49204#file-test_sparse_dok_indexer-py
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+# http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import numpy as np
 from sparse import DOK
 
