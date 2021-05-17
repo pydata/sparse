d581636d58ef797db7255363225c520e676ea705
Hameer Abbasi <einstein.edison@gmail.com>
2021-05-17 14:43:56 +0200

    Swap argument numbers

--- benchmarks/benchmark_matmul.py
+++ benchmarks/benchmark_matmul.py
@@ -10,9 +10,9 @@ class Matmul_Sparse:
         self.x = sparse.random((100, 100), density=0.01, format=p)
         self.y = sparse.random((100, 100), density=0.01, format=p)
 
-        if(dens_arg==1):
+        if(dens_arg==0):
             self.x = self.x.todense()
-        elif(dens_arg==0):
+        elif(dens_arg==1):
             self.y = self.y.todense()
 
         self.x @ self.y
