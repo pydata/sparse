467a57e77f1be49b52d58b581905ea48776e1502
Sayandip Halder <sayandiph4@gmail.com>
2021-05-17 17:37:17 +0530

    test2

--- benchmarks/benchmark_matmul.py
+++ benchmarks/benchmark_matmul.py
@@ -12,7 +12,7 @@ class Matmul_Sparse:
 
         if(dens_arg==1):
             self.x = self.x.todense()
-        elif(dens_arg==0)
+        elif(dens_arg==0):
             self.y = self.y.todense()
 
         self.x @ self.y
