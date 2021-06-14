765e2972fccf1dc35f513aa2f8ba9ff8b7d13b09
Dylan Aïssi <daissi@debian.org>
2021-06-14 19:17:26 +0200

    Skip doctest on 32bits arch (#486)
    * Skip doctest on 32bits arch
    
    * Move test for 32bits in the first 'if'
    
    * Replace OR by AND
    
    * Reformatting with black
    
    Co-authored-by: Dylan Aïssi <dylan.aissi@collabora.com>

--- sparse/tests/conftest.py
+++ sparse/tests/conftest.py
@@ -1,6 +1,7 @@
 import platform
+import numpy.distutils.system_info as sysinfo
 
 
 def pytest_cmdline_preparse(args):
-    if platform.system() != "Windows":
+    if platform.system() != "Windows" and sysinfo.platform_bits != 32:
         args.append("--doctest-modules")
