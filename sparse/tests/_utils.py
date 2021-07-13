broadcasting fixes

sparse/tests/_utils.py
@@ -1,7 +1,12 @@
 from numbers import Rational
 from hypothesis import given, strategies as st
 from hypothesis.strategies import data, composite
-from hypothesis.extra.numpy import array_shapes, basic_indices, broadcastable_shapes
+from hypothesis.extra.numpy import (
+    array_shapes,
+    basic_indices,
+    broadcastable_shapes,
+    mutually_broadcastable_shapes,
+)
 import numpy as np
 import sparse
 import scipy.sparse
@@ -95,9 +100,9 @@ def gen_setitem_val_err(draw):
 @composite
 def gen_transpose(draw):
     a = draw(st.lists(st.integers(min_value=1, max_value=5), min_size=2, max_size=4))
-    if len(a) is 2:
+    if len(a) == 2:
         b = draw(st.sampled_from([(1, 0), (0, 1)]))
-    elif len(a) is 3:
+    elif len(a) == 3:
         b = draw(st.sampled_from([(0, 2, 1), (2, 0, 1), (1, 2, 0)]))
     else:
         b = draw(st.sampled_from([(0, 3, 2, 1), (1, 0, 3, 2), (3, 2, 0, 1)]))
@@ -133,15 +138,14 @@ def gen_reductions(draw, function=False):
 
 
 @composite
-def gen_broadcast_shape(draw):
-    shape1 = draw(
-        st.lists(st.integers(min_value=2, max_value=5), min_size=2, max_size=3).map(
-            tuple
+def gen_broadcast_shape(draw, num_shapes=2):
+    shape_1, shape_2 = draw(
+        mutually_broadcastable_shapes(
+            num_shapes=num_shapes, max_dims=2, min_side=2, max_side=5
         )
-    )
-    shape2 = draw(broadcastable_shapes(shape1, min_side=3))
+    ).input_shapes
 
-    return shape1, shape2
+    return shape_1, shape_2
 
 
 @composite
@@ -183,19 +187,17 @@ def gen_matmul_warning(draw):
 
 
 @composite
-def gen_broadcast_shape_dot(draw):
-    a_shape = draw(
-        st.lists(st.integers(min_value=2, max_value=5), min_size=2, max_size=2).map(
-            tuple
+def gen_broadcast_shape_dot(draw, max_dims=4):
+    a_shape, b_shape = draw(
+        mutually_broadcastable_shapes(
+            signature=np.matmul.signature, max_dims=2, min_side=2, max_side=5
         )
-    )
-    b_shape = draw(broadcastable_shapes(a_shape, min_dims=2, max_dims=2))
+    ).input_shapes
+    if len(a_shape) != 1 and len(b_shape) != 1:
+        a_shape = draw(array_shapes(min_side=2, max_side=5, max_dims=2)) + a_shape
+        b_shape = draw(array_shapes(min_side=2, max_side=5, max_dims=2)) + b_shape
 
-    a_shape = list(a_shape)
-    a_shape[-1] = b_shape[0]
-    a_shape = tuple(a_shape)
-
-    return a_shape, b_shape
+    return tuple(a_shape), tuple(b_shape)
 
 
 @composite
@@ -323,3 +325,18 @@ def gen_advanced_indexing(draw):
     )
 
     return index, s
+
+
+@composite
+def gen_random_seed(draw):
+    n = draw(st.integers(min_value=0, max_value=99))
+    return n
+
+
+@composite
+def gen_matmul_shapes(draw):
+    return draw(
+        mutually_broadcastable_shapes(
+            signature=np.matmul.signature, max_dims=4, min_side=2, max_side=5
+        )
+    ).input_shapes

sparse/tests/test_dot.py
@@ -1,7 +1,13 @@
 import numpy as np
 import pytest
 from hypothesis import settings, given, strategies as st
-from _utils import gen_matmul_warning, gen_broadcast_shape_dot, gen_sparse_random
+from _utils import (
+    gen_matmul_warning,
+    gen_broadcast_shape_dot,
+    gen_sparse_random,
+    gen_random_seed,
+    gen_matmul_shapes,
+)
 import scipy.sparse
 import scipy.stats
 
@@ -205,6 +211,29 @@ def test_dot(ab, a_format, b_format, a_kwargs, b_kwargs):
     assert_eq(e, sparse.dot(sa, sb))
     assert_eq(e, sparse.dot(a, sb))
     assert_eq(e, sparse.dot(a, sb))
+
+
+@settings(deadline=None)
+@given(ab=gen_matmul_shapes())
+@pytest.mark.parametrize(
+    "a_format, a_kwargs",
+    [*gen_for_format("coo"), *gen_for_format("gcxs")],
+)
+@pytest.mark.parametrize(
+    "b_format, b_kwargs",
+    [*gen_for_format("coo"), *gen_for_format("gcxs")],
+)
+def test_matmul_2(ab, a_format, b_format, a_kwargs, b_kwargs):
+    a_shape, b_shape = ab
+    if len(a_shape) == 1:
+        a_kwargs = {}
+    if len(b_shape) == 1:
+        b_kwargs = {}
+    sa = sparse.random(a_shape, density=0.5, format=a_format, **a_kwargs)
+    sb = sparse.random(b_shape, density=0.5, format=b_format, **b_kwargs)
+
+    a = sa.todense()
+    b = sb.todense()
 
     # Basic equivalences
     e = operator.matmul(a, b)