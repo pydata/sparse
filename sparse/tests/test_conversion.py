f910cd0cb54803d29bbc560742ea11b16ab6e70a
Hameer Abbasi <hameerabbasi@yahoo.com>
2021-10-21 07:16:02 +0200

    Fix full_like with GCXS input.

--- sparse/tests/test_conversion.py
+++ sparse/tests/test_conversion.py
@@ -18,3 +18,10 @@ def test_conversion(format1, format2):
     x = sparse.random((10, 10), density=0.5, format=format1, fill_value=0.5)
     y = x.asformat(format2)
     assert_eq(x, y)
+
+
+def test_extra_kwargs():
+    x = sparse.full((2, 2), 1, format="gcxs", compressed_axes=[1])
+    y = sparse.full_like(x, 1)
+
+    assert_eq(x, y)
