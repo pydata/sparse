6937aecdb488042a3f3cc5cac26e7a4c952d9617
Sultan Orazbayev <contact@econpoint.com>
2023-01-13 12:46:41 +0600

    Add .format support to GCXS class

--- sparse/_compressed/compressed.py
+++ sparse/_compressed/compressed.py
@@ -278,6 +278,28 @@ class GCXS(SparseArray, NDArrayOperatorsMixin):
         return self.data.shape[0]
 
     @property
+    def format(self):
+        """
+        The storage format of this array.
+        Returns
+        -------
+        str
+            The storage format of this array.
+        See Also
+        -------
+        COO.format : Equivalent :obj:`COO` array property.
+        GCXS.format : Equivalent :obj:`GCXS` array property.
+        scipy.sparse.dok_matrix.format : The Scipy equivalent property.
+        Examples
+        -------
+        >>> import sparse
+        >>> s = sparse.random((5,5), density=0.2, format='gcxs')
+        >>> s.format
+        'gcxs'
+        """
+        return "gcxs"
+
+    @property
     def nbytes(self):
         """
         The number of bytes taken up by this object. Note that for small arrays,
