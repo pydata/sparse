08aab728fa9589d1a8437f22290f9d9dd45a94b5
Hameer Abbasi <einstein.edison@gmail.com>
2024-01-29 15:09:41 +0100

    Remove dynamic options.

--- sparse/tests/conftest.py
+++ sparse/tests/conftest.py
@@ -1,14 +1,4 @@
-import platform
-import sys
-
 import pytest
-
-is_64bits = sys.maxsize > 2**32
-
-
-def pytest_cmdline_preparse(args):
-    if platform.system() != "Windows" and not is_64bits:
-        args.append("--doctest-modules")
 
 
 @pytest.fixture(scope="session")
