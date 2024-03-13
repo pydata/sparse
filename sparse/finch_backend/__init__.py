50fbf4a6a67266fd050efa8b36e39e18dc44edb7
Mateusz Sokół <mat646@gmail.com>
2024-03-13 11:39:14 +0100

    JuliaCall test

--- sparse/finch_backend/__init__.py
+++ sparse/finch_backend/__init__.py
@@ -1,9 +1,18 @@
+import juliapkg
+
+juliapkg.add("Finch", "9177782c-1635-4eb9-9bfb-d9dfa25e6bce", version="0.6.16")
+juliapkg.resolve()
+
+from juliacall import Main as jl  # noqa: E402
+
+jl.seval("using Finch")
+
 try:
     import finch  # noqa: F401
 except ModuleNotFoundError:
     raise ImportError("Finch not installed. Run `pip install sparse[finch]` to enable Finch backend") from None
 
-from finch import Tensor, astype, permute_dims
+from finch import Tensor, astype, permute_dims  # noqa: E402
 
 __all__ = ["Tensor", "permute_dims", "astype"]
 
--- sparse/tests/test_backends.py
+++ sparse/tests/test_backends.py
@@ -6,15 +6,33 @@ import numpy as np
 import scipy.sparse as sp
 
 
-def test_backend_contex_manager(backend):
-    if backend == sparse.BackendType.Finch:
-        with pytest.raises(NotImplementedError):
-            sparse.COO.from_numpy(np.eye(5))
-    else:
-        sparse.COO.from_numpy(np.eye(5))
+# def test_backend_contex_manager(backend):
+#     if backend == sparse.BackendType.Finch:
+#         with pytest.raises(NotImplementedError):
+#             sparse.COO.from_numpy(np.eye(5))
+#     else:
+#         sparse.COO.from_numpy(np.eye(5))
 
 
 def test_finch_backend():
+    np_eye = np.eye(5)
+    sp_arr = sp.csr_matrix(np_eye)
+
+    with sparse.Backend(backend=sparse.BackendType.Finch):
+        finch_dense = sparse.Tensor(np_eye)
+
+        assert np.shares_memory(finch_dense.todense(), np_eye)
+
+        finch_arr = sparse.Tensor(sp_arr)
+
+        np.testing.assert_equal(finch_arr.todense(), np_eye)
+
+        transposed = sparse.permute_dims(finch_arr, (1, 0))
+
+        np.testing.assert_equal(transposed.todense(), np_eye.T)
+
+
+def test_finch_backend_2():
     np_eye = np.eye(5)
     sp_arr = sp.csr_matrix(np_eye)
 
