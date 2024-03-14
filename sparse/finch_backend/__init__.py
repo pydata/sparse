adfdf04b427ce2e52aea9100e3400e8c0679c6e2
Mateusz Sokół <8431159+mtsokol@users.noreply.github.com>
2024-03-14 17:11:08 +0100

    API: Initial `finch_backend` (#649)
    * API: Initial finch backend
    
    Co-authored-by: Hameer Abbasi <2190658+hameerabbasi@users.noreply.github.com>

--- sparse/finch_backend/__init__.py
+++ sparse/finch_backend/__init__.py
@@ -1,4 +1,11 @@
-Tensor = ()
+try:
+    import finch  # noqa: F401
+except ModuleNotFoundError as e:
+    raise ImportError("Finch not installed. Run `pip install sparse[finch]` to enable Finch backend") from e
+
+from finch import Tensor, astype, permute_dims
+
+__all__ = ["Tensor", "astype", "permute_dims"]
 
 
 class COO:
