8ec2e62454b397db0798536a9b09ff02174ba5ab
Mateusz Sokół <mat646@gmail.com>
2024-03-22 11:00:43 +0100

    Test finch in asv

--- benchmarks/benchmark_coo.py
+++ benchmarks/benchmark_coo.py
@@ -1,27 +1,25 @@
 import sparse
 
-import numpy as np
-
-
-class MatrixMultiplySuite:
-    def setup(self):
-        rng = np.random.default_rng(0)
-        self.x = sparse.random((100, 100), density=0.01, random_state=rng)
-        self.y = sparse.random((100, 100), density=0.01, random_state=rng)
-
-        self.x @ self.y  # Numba compilation
-
-    def time_matmul(self):
-        self.x @ self.y
+import os
 
 
 class ElemwiseSuite:
-    def setup(self):
-        rng = np.random.default_rng(0)
-        self.x = sparse.random((100, 100, 100), density=0.01, random_state=rng)
-        self.y = sparse.random((100, 100, 100), density=0.01, random_state=rng)
+    timeout = 120.0
+    warmup_time = 10.0
+    repeat = 10
 
-        self.x + self.y  # Numba compilation
+    def setup(self):
+        self.x = sparse.random((100, 100, 100), density=0.01, random_state=42)
+        self.y = sparse.random((100, 100, 100), density=0.01, random_state=42)
+
+        if os.environ[sparse._ENV_VAR_NAME] == "Finch":
+            import finch
+            storage = finch.Storage(finch.Dense(finch.SparseList(finch.SparseList(finch.Element(0.0)))))
+            self.x = self.x.to_device(storage)
+            self.y = self.y.to_device(storage)
+
+        self.x + self.y  # compilation
+        self.x * self.y  # compilation
 
     def time_add(self):
         self.x + self.y
@@ -30,40 +28,46 @@ class ElemwiseSuite:
         self.x * self.y
 
 
-class ElemwiseBroadcastingSuite:
-    def setup(self):
-        rng = np.random.default_rng(0)
-        self.x = sparse.random((100, 1, 100), density=0.01, random_state=rng)
-        self.y = sparse.random((100, 100), density=0.01, random_state=rng)
+# class ElemwiseBroadcastingSuite:
+#     timeout = 120.0
+#     warmup_time = 5.0
+#     repeat = 1
 
-    def time_add(self):
-        self.x + self.y
+#     def setup(self):
+#         self.x = sparse.random((1, 100, 100), density=0.01, random_state=42)
+#         self.y = sparse.random((100, 100), density=0.01, random_state=42)
 
-    def time_mul(self):
-        self.x * self.y
+#         self.x + self.y  # compilation
+#         self.x * self.y  # compilation
+
+#     def time_add(self):
+#         self.x + self.y
+
+#     def time_mul(self):
+#         self.x * self.y
 
 
-class IndexingSuite:
-    def setup(self):
-        rng = np.random.default_rng(0)
-        self.index = rng.integers(0, 100, 50)
-        self.x = sparse.random((100, 100, 100), density=0.01, random_state=rng)
+# class IndexingSuite:
+#     def setup(self):
+#         rng = np.random.default_rng(0)
+#         self.index = rng.integers(0, 100, 50)
+#         self.x = sparse.random((100, 100, 100), density=0.01, random_state=rng)
 
-        # Numba compilation
-        self.x[5]
-        self.x[self.index]
+#         # Numba compilation
+#         self.x[5]
+#         self.x[self.index]
 
-    def time_index_scalar(self):
-        self.x[5, 5, 5]
+#     def time_index_scalar(self):
+#         self.x[5, 5, 5]
 
-    def time_index_slice(self):
-        self.x[:50]
+#     def time_index_slice(self):
+#         self.x[:50]
 
-    def time_index_slice2(self):
-        self.x[:50, :50]
+#     def time_index_slice2(self):
+#         self.x[:50, :50]
 
-    def time_index_slice3(self):
-        self.x[:50, :50, :50]
+#     def time_index_slice3(self):
+#         self.x[:50, :50, :50]
 
-    def time_index_fancy(self):
-        self.x[self.index]
+#     def time_index_fancy(self):
+#         self.x[self.index]
