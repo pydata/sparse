61789c3552ccd9e54e3e86a0bbd1ab280ea4353e
Mateusz Sokół <mat646@gmail.com>
2024-05-08 14:54:21 +0200

    Apply review comments

--- examples/sddmm_example.py
+++ examples/sddmm_example.py
@@ -5,28 +5,32 @@ import sparse
 import numpy as np
 import scipy.sparse as sps
 
-LEN = 1000
+LEN = 10000
 DENSITY = 0.0001
-ITERS = 3
-rng = np.random.default_rng()
+ITERS = 5
+rng = np.random.default_rng(0)
 
 
 def benchmark(func, info, args):
     print(info)
     start = time.time()
-    for i in range(ITERS):
-        print(f"iter: {i}")
+    for _ in range(ITERS):
         func(*args)
     elapsed = time.time() - start
     print(f"Took {elapsed / ITERS} s.\n")
 
 
 if __name__ == "__main__":
+    a_sps = rng.random((LEN, LEN - 10)) * 10
+    b_sps = rng.random((LEN, LEN - 10)) * 10
+    s_sps = sps.random(LEN, LEN, format="coo", density=DENSITY, random_state=rng) * 10
+    s_sps.sum_duplicates()
+
     # Finch
     with sparse.Backend(backend=sparse.BackendType.Finch):
-        s = sparse.random((LEN, LEN), density=DENSITY, random_state=rng)
-        a = sparse.asarray(np.array(rng.random((LEN, LEN)), order="F"))
-        b = sparse.asarray(np.array(rng.random((LEN, LEN)), order="F"))
+        s = sparse.asarray(s_sps)
+        a = sparse.asarray(np.array(a_sps, order="F"))
+        b = sparse.asarray(np.array(b_sps, order="F"))
 
         @sparse.compiled
         def sddmm_finch(s, a, b):
@@ -40,12 +44,12 @@ if __name__ == "__main__":
 
     # Numba
     with sparse.Backend(backend=sparse.BackendType.Numba):
-        s = sparse.random((LEN, LEN), density=DENSITY, random_state=rng)
-        a = rng.random((LEN, LEN))
-        b = rng.random((LEN, LEN))
+        s = sparse.asarray(s_sps)
+        a = a_sps
+        b = b_sps
 
         def sddmm_numba(s, a, b):
-            return sparse.sum(s[:, :, None] * (a[:, None, :] * b[None, :, :]), axis=-1)
+            return s * (a @ b.T)
 
         # Compile
         result_numba = sddmm_numba(s, a, b)
@@ -55,11 +59,16 @@ if __name__ == "__main__":
 
     # SciPy
     def sddmm_scipy(s, a, b):
-        return s * (a @ b)
+        return s.multiply(a @ b.T)
 
-    s = sps.random_array((LEN, LEN), format="coo", density=DENSITY, random_state=rng)
-    a = rng.random((LEN, LEN))
-    b = rng.random((LEN, LEN))
+    s = s_sps.asformat("csr")
+    a = a_sps
+    b = b_sps
 
+    result_scipy = sddmm_scipy(s, a, b)
     # Benchmark
     benchmark(sddmm_scipy, info="SciPy", args=[s, a, b])
+
+    np.testing.assert_allclose(result_numba.todense(), result_scipy.toarray())
+    np.testing.assert_allclose(result_finch.todense(), result_numba.todense())
+    np.testing.assert_allclose(result_finch.todense(), result_scipy.toarray())
