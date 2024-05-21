b0f1a819b0b40b27e93968ef3f9c2bc005c5c627
Mateusz Sokół <8431159+mtsokol@users.noreply.github.com>
2024-05-21 17:17:56 +0200

    Add tests for `scipy.sparse.csgraph` and `scipy.sparse.linalg` (#683)
    * Add tests for scipy.sparse.csgraph and linalg
    
    * Separate tests
    
    * Add parameters

--- sparse/tests/conftest.py
+++ sparse/tests/conftest.py
@@ -2,8 +2,23 @@ import sparse
 
 import pytest
 
+import numpy as np
+
 
 @pytest.fixture(scope="session", params=[sparse.BackendType.Numba, sparse.BackendType.Finch])
 def backend(request):
     with sparse.Backend(backend=request.param):
         yield request.param
+
+
+@pytest.fixture(scope="module")
+def graph():
+    return np.array(
+        [
+            [0, 1, 1, 0, 0],
+            [0, 0, 1, 0, 1],
+            [0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 1],
+            [0, 1, 0, 1, 0],
+        ]
+    )
