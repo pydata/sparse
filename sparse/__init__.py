50409b15afda22bcba5321bde1cc5b6781ad6014
Hameer Abbasi <2190658+hameerabbasi@users.noreply.github.com>
2024-06-11 10:22:03 +0200

    Detect coverage better.

--- sparse/__init__.py
+++ sparse/__init__.py
@@ -16,19 +16,19 @@ _ENV_VAR_NAME = "SPARSE_BACKEND"
 
 if _ENV_VAR_NAME in os.environ:
     warnings.warn("Selectable backends feature in `sparse` might change in the future.", FutureWarning, stacklevel=1)
-    backend_name = os.environ[_ENV_VAR_NAME]
+    _backend_name = os.environ[_ENV_VAR_NAME]
 else:
-    backend_name = BackendType.Numba.value
+    _backend_name = BackendType.Numba.value
 
-if backend_name not in {BackendType.Numba.value, BackendType.Finch.value}:
-    warnings.warn(f"Invalid backend identifier: {backend_name}. Selecting Numba backend.", UserWarning, stacklevel=1)
-    backend = BackendType.Numba
+if _backend_name not in {BackendType.Numba.value, BackendType.Finch.value}:
+    warnings.warn(f"Invalid backend identifier: {_backend_name}. Selecting Numba backend.", UserWarning, stacklevel=1)
+    BACKEND = BackendType.Numba
 else:
-    backend = BackendType[backend_name]
+    BACKEND = BackendType[_backend_name]
 
-del backend_name
+del _backend_name
 
-if backend == BackendType.Finch:
+if BackendType.Finch == BACKEND:
     from sparse.finch_backend import *  # noqa: F403
     from sparse.finch_backend import __all__
 else:
