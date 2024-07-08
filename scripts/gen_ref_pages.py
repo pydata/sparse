c5a476f688c42dffc791233c6bf50ce07b9478df
Dea María Léon <deamarialeon@gmail.com>
2024-07-08 09:14:06 +0200

    wip

--- scripts/gen_ref_pages.py
+++ scripts/gen_ref_pages.py
@@ -7,7 +7,7 @@ import mkdocs_gen_files
 nav = mkdocs_gen_files.Nav()
 
 root = Path(__file__).parent.parent
-src = root / "sparse/numba_backend/_coo"
+src = root / "sparse/numba_backend"
 
 for path in sorted(src.rglob("*.py")):
     module_path = path.relative_to(root).with_suffix("")
@@ -15,13 +15,24 @@ for path in sorted(src.rglob("*.py")):
     full_doc_path = Path("api", doc_path)
 
     parts = tuple(module_path.parts)
-
-    if parts[-1] == "common" or parts[-1] == "core":
-        nav[parts] = doc_path.as_posix()
-        with mkdocs_gen_files.open(full_doc_path, "w") as fd:
-            identifier = ".".join(parts)
-            print("::: " + identifier, file=fd)
-
+    if "tests" in parts:
+        continue
+    #if parts[-1] == "common" or parts[-1] == "core":
+#
+    #    nav[parts] = doc_path.as_posix()
+    #    with mkdocs_gen_files.open(full_doc_path, "w") as fd:
+    #        identifier = ".".join(parts)
+    #        print("::: " + identifier, file=fd)
+    if parts[-1] == "__init__":
+        parts = parts[:-1]
+        doc_path = doc_path.with_name("index.md")
+        full_doc_path = full_doc_path.with_name("index.md")
+    elif parts[-1] == "__main__":
+        continue
+    with mkdocs_gen_files.open(full_doc_path, "w") as fd:
+        identifier = ".".join(parts)
+        print("::: " + identifier, file=fd)
+    nav[parts] = doc_path.as_posix()
     mkdocs_gen_files.set_edit_path(full_doc_path, path.relative_to(root))
 
 with mkdocs_gen_files.open("api/SUMMARY.md", "w") as nav_file:
