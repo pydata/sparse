c2ed8a05ab8ba7926504f0ac9ac7ba84172f0a5e
Dea María Léon <deamarialeon@gmail.com>
2024-07-10 13:43:17 +0200

    gen_ref_pages using __module__

--- scripts/gen_ref_pages.py
+++ scripts/gen_ref_pages.py
@@ -3,17 +3,15 @@
 from pathlib import Path
 
 import mkdocs_gen_files
+import sparse
 
-root = Path(__file__).parent.parent
+nav = mkdocs_gen_files.Nav()
 
-x = ["COO"]
-# for i in sparse.__all__:
-for i in x:
-    if i in ["acos", "acosh", "add", "asin", "asinh"]:
+for item in dir(sparse):
+    if (
+        item.startswith("_") or
+        not getattr(getattr(sparse, item), "__module__", "").startswith("sparse")
+    ):
         continue
-    file_name = i + ".md"
-    full_doc_path = Path("api/" + file_name)
-    print(file_name)
-    with mkdocs_gen_files.open(full_doc_path, "w") as file_name:
-        print("::: sparse." + i, file=file_name)
-    mkdocs_gen_files.set_edit_path(full_doc_path, root)
+    with mkdocs_gen_files.open(Path("api", f"{item}.md"), "w") as fd:
+        print("::: " + f"sparse.{item}", file=fd)
