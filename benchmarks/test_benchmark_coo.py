3ccb80d41cdb7606d854a013e4e28497ab438d8f
Dea María Léon <deamarialeon@gmail.com>
2024-08-05 12:53:21 +0200

    changed size

--- benchmarks/test_benchmark_coo.py
+++ benchmarks/test_benchmark_coo.py
@@ -5,8 +5,8 @@ import numpy as np
 
 def test_matmul(benchmark):
     rng = np.random.default_rng(seed=42)
-    x = sparse.random((3, 3), density=0.01, random_state=rng)
-    y = sparse.random((3, 3), density=0.01, random_state=rng)
+    x = sparse.random((40, 40), density=0.01, random_state=rng)
+    y = sparse.random((40, 40), density=0.01, random_state=rng)
 
     x @ y  # Numba compilation
 
