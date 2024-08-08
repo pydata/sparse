61641b1401890500a5e5f4af36e2ac2f86d1e874
Dea María Léon <deamarialeon@gmail.com>
2024-08-08 13:09:05 +0200

    Wip benchmarks

--- benchmarks/test_benchmark_coo.py
+++ benchmarks/test_benchmark_coo.py
@@ -1,15 +1,33 @@
+import operator
+
 import sparse
+
+import pytest
 
 import numpy as np
 
+DENSITY = 0.01
+SEED = 42
 
-def test_matmul(benchmark):
-    rng = np.random.default_rng(seed=42)
-    x = sparse.random((40, 40), density=0.01, random_state=rng)
-    y = sparse.random((40, 40), density=0.01, random_state=rng)
 
-    x @ y  # Numba compilation
+@pytest.fixture(scope="module")
+@pytest.mark.parametrize("side", [100, 500, 1000])
+@pytest.mark.parametrize("rank", [1, 2, 3, 4])
+def elemwise_args(side: int, rank: int):
+    if side**rank >= 2**26:
+        pytest.skip()
+    rng = np.random.default_rng(seed=SEED)
+    shape = (side,) * rank
+    x = sparse.random(shape, density=DENSITY, random_state=rng)
+    y = sparse.random(shape, density=DENSITY, random_state=rng)
+    return x, y
+
+
+@pytest.mark.parametrize("f", [operator.add, operator.mul])
+def test_elemwise(benchmark, f, elemwise_args):
+    x, y = elemwise_args
+    f(x, y)
 
     @benchmark
-    def test_matmul():
-        x @ y
+    def bench():
+        f(x, y)
