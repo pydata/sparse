95353a56d44ca49839043128f29173707482fb64
Dea María Léon <deamarialeon@gmail.com>
2024-08-18 17:52:26 +0200

    after pre-commit

--- benchmarks/test_elemwise.py
+++ benchmarks/test_elemwise.py
@@ -1,18 +1,16 @@
-import itertools
+import importlib
 import operator
+import os
 
 import sparse
 
 import pytest
 
 import numpy as np
-
-import os
-import importlib
-
 import scipy.sparse as sps
 
 DENSITY = 0.001
+
 
 def get_test_id(side):
     return f"{side=}"
@@ -34,13 +32,12 @@ def elemwise_args(request, seed, max_size):
 @pytest.mark.parametrize("f", [operator.add, operator.mul, operator.ge])
 @pytest.mark.parametrize("backend", ["SciPy", "Numba", "Finch"])
 def test_elemwise(benchmark, f, backend, elemwise_args):
-    
     if backend in ["Numba", "Finch"]:
         os.environ[sparse._ENV_VAR_NAME] = backend
         importlib.reload(sparse)
-    
+
     s1_sps, s2_sps = elemwise_args
-    
+
     if backend == "SciPy":
         s1 = s1_sps
         s2 = s2_sps
@@ -50,9 +47,9 @@ def test_elemwise(benchmark, f, backend, elemwise_args):
     elif backend == "Finch":
         s1 = sparse.asarray(s1_sps.asformat("csc"), format="csc")
         s2 = sparse.asarray(s2_sps.asformat("csc"), format="csc")
-    
+
     f(s1, s2)
-    
+
     @benchmark
     def bench():
         f(s1, s2)
