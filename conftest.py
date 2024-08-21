4447505b2d0c144ff9e45e408e78eeddceaf1937
Hameer Abbasi <2190658+hameerabbasi@users.noreply.github.com>
2024-08-21 08:47:52 +0200

    Attempted fix for CI.

--- conftest.py
+++ conftest.py
@@ -7,8 +7,5 @@ def add_doctest_modules(doctest_namespace):
 
     import numpy as np
 
-    if sparse._BackendType.Numba != sparse._BACKEND:
-        pytest.skip()
-
     doctest_namespace["np"] = np
     doctest_namespace["sparse"] = sparse
