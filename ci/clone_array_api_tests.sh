bb8b57191d545438fa27432d8ac67274e7d48928
Hameer Abbasi <2190658+hameerabbasi@users.noreply.github.com>
2025-01-09 11:48:54 +0100

    Bug-fixes

--- ci/clone_array_api_tests.sh
+++ ci/clone_array_api_tests.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
 set -euxo pipefail
 
-if [ ! -d "../array-api-tests" ]; then
-  git clone https://github.com/data-apis/array-api-tests.git "../array-api-tests"
+ARRAY_API_TESTS_DIR="${ARRAY_API_TESTS_DIR:-"../array-api-tests"}"
+if [ ! -d "$ARRAY_API_TESTS_DIR" ]; then
+  git clone --recursive https://github.com/data-apis/array-api-tests.git "$ARRAY_API_TESTS_DIR"
 fi
-git --git-dir="../array-api-tests/.git" fetch
-git --git-dir="../array-api-tests/.git" checkout $(cat "ci/array-api-tests-rev.txt")
+
+git --git-dir="$ARRAY_API_TESTS_DIR/.git" --work-tree "$ARRAY_API_TESTS_DIR" clean -xddf
+git --git-dir="$ARRAY_API_TESTS_DIR/.git" --work-tree "$ARRAY_API_TESTS_DIR" fetch
+git --git-dir="$ARRAY_API_TESTS_DIR/.git" --work-tree "$ARRAY_API_TESTS_DIR" reset --hard $(cat "ci/array-api-tests-rev.txt")
--- ci/test_array_api.sh
+++ ci/test_array_api.sh
@@ -6,4 +6,4 @@ source ci/clone_array_api_tests.sh
 if [ "${SPARSE_BACKEND}" = "Finch" ]; then
     python -c 'import finch'
 fi
-ARRAY_API_TESTS_MODULE="sparse" pytest ../array-api-tests/array_api_tests/ -v -c ../array-api-tests/pytest.ini --ci --max-examples=2 --derandomize --disable-deadline -o xfail_strict=True -n auto --xfails-file ../sparse/ci/${SPARSE_BACKEND}-array-api-xfails.txt --skips-file ../sparse/ci/${SPARSE_BACKEND}-array-api-skips.txt
+ARRAY_API_TESTS_MODULE="sparse" pytest "$ARRAY_API_TESTS_DIR/array_api_tests/" -v -c "$ARRAY_API_TESTS_DIR/pytest.ini" --ci --max-examples=2 --derandomize --disable-deadline -o xfail_strict=True -n auto --xfails-file ../sparse/ci/${SPARSE_BACKEND}-array-api-xfails.txt --skips-file ../sparse/ci/${SPARSE_BACKEND}-array-api-skips.txt
