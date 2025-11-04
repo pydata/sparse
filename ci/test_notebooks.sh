a2044dc55417cbd856ea0488772d7adc7dabeade
Pradyot Ranjan <99216956+pradyotRanjan@users.noreply.github.com>
2025-11-04 23:33:57 +0530

    fixing doc path
    Signed-off-by: Pradyot Ranjan <99216956+pradyotRanjan@users.noreply.github.com>

--- ci/test_notebooks.sh
+++ ci/test_notebooks.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 set -euxo pipefail
 
-CI_MODE=1 pytest -n 4 --nbmake --nbmake-timeout=600 ./docs/examples/*.ipynb
+CI_MODE=1 pytest -n 4 --nbmake --nbmake-timeout=600 ./examples/*.ipynb
