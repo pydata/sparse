97eb95ac1d3589b49c6a91cb922126f23271daf7
Hameer Abbasi <2190658+hameerabbasi@users.noreply.github.com>
2026-07-01 13:27:14 +0200

    Add flaky test to skips.

---
+++ scripts/test_notebooks.sh
@@ -0,0 +1,7 @@
+for example in $(find ./examples/ -iname '*.ipynb'); do
+  if [ ! "$example" =~ "finch" ]; then
+    pytest -n 4 --nbmake --nbmake-timeout=600 "$example"
+  else
+    echo "Skipping finch example: $example"
+  fi
+done
